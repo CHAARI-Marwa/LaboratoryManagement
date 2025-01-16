dfd88a10d86161369c5452cde9cd1e6f211165d0
Marwa Chaari <marwachaarie@gmail.com>
2025-01-16 22:41:37 +0100

    Link between membercomponent and memberservice

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -3,6 +3,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { Member } from 'src/models/member';
 import { MemberService } from 'src/services/member.service';
 import { ConfirmComponent } from '../confirm/confirm.component';
+import { Etudiant } from 'src/models/Etudiant';
+import { EnseignantChercheur } from 'src/models/EnseignantChercheur';
 
 @Component({
   selector: 'app-member',
@@ -11,47 +13,49 @@ import { ConfirmComponent } from '../confirm/confirm.component';
 })
 export class MemberComponent implements OnInit{
 
-  constructor(private MS : MemberService, private dialog : MatDialog){
+  dataSource: Member[] = [];
+  dataEtudiant: Etudiant[] = [];
+  dataEnseignant: EnseignantChercheur[] = [];
 
-  }
+  constructor(private ms: MemberService, private dialog: MatDialog) {}
 
-
-  dataSource : Member[]=[]
-
-
-  //se load avant le constructeur
-  ngOnInit():void
-  //injection de dependances : me permet d'utiliser le service dans les composants ou dans le s autres services
-  //elle consiste a crere une instance privé du service dans le constructeur du composant 
-  {
-    //appeler la fonction du service getAllMembers
-    //attendere le resultat
-    //une fois on recoit le res => l'affecter dans datasource
-    this.MS.getAllMembers().subscribe((/*le retour va etre stocké ici dans cette variable*//*tkhdem ken houni n'est pas visible à l'exterieur*/ response)=>{
-      /*une fois t3abet variable chnou naaml post action post resultat*/
-      this.dataSource=response
-    })
-  }
-
-  delete(id:string):void{
-    //1.lancer la boite(confirmcomponent)
-    const dialogref=this.dialog.open(ConfirmComponent);
-    //apres l'ouverture je reste subscriber et attendre la reponse de delete
-    //2.attendre le resu de l'user
-    dialogref.afterClosed().subscribe((response)=>{
-      if (response)
+  displayedColumns: string[] = ['cin', 'nom', 'prenom', 'dateNaissance', 'cv', 'email', 'dateInscription', 'diplome', 'encadrant', 'delete', 'edit'];
+  displayedColumnsEnseignants: string[] = ['cin', 'nom', 'prenom', 'dateNaissance', 'cv', 'email', 'dateInscription', 'grade', 'etablissement', 'delete', 'edit'];
+  ngOnInit(): void {
     
-
-    //3. si l'util a fait le click sur confirm
-    this.MS.deleteMember(id).subscribe(()=>{
-      //action apres avoir éfacer
-      this.MS.getAllMembers().subscribe((response)=>{
-        this.dataSource=response
-      })
-    })
-  })
+    this.getEtudiants();
+    this.GetEnseignant();
   }
 
-  displayedColumns: string[] = ['ID', 'CIN', 'Name', 'CV', 'Type', 'createDate', 'edit', 'delete'];
+  getEtudiants(): void {
+    this.ms.getAllStudents().subscribe((response) => {
+      this.dataEtudiant = response;
+    });
+  }
 
+  GetEnseignant(): void {
+    this.ms.getAllTeachers().subscribe((resultat) => {
+      this.dataEnseignant = resultat;
+      console.log(resultat);
+    });
+  }
+
+  delete(id: string): void {
+    console.log(id);
+
+    // Lancer la boîte de dialogue de confirmation
+    const dialogRef = this.dialog.open(ConfirmComponent);
+
+    // Attendre la réponse de l'utilisateur
+    dialogRef.afterClosed().subscribe((response) => {
+      if (response) {
+        this.ms.delete(id).subscribe(() => {
+          // Réexécuter la requête pour actualiser les données après suppression
+          this.ms.getAllMembers().subscribe((response) => {
+            this.dataSource = response;
+          });
+        });
+      }
+    });
+  }
 }
--- src/services/member.service.ts
+++ src/services/member.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { EnseignantChercheur } from 'src/models/EnseignantChercheur';
+import { Etudiant } from 'src/models/Etudiant';
 import { Member } from 'src/models/member';
 
 //le service se déclare capable d'etre injecté  on peut l'utiliser dans autre services et components 
@@ -16,21 +18,40 @@ export class MemberService {
   //quand je lance la req angular lance un thread de type observable: 3 partie de l'observable subscriber w observer w notification
   //service houwa subscriber
   //json server c l'observable and notification houwa tableau de membre 
-  getAllMembers():Observable<Member[]>
+  
+  getAllMembers(): Observable<Member[] >
+
   {
-    //envoie d'une requete en mode get
-    return this.http.get<Member[]>('http://localhost:3000/members')
+    
+    return this.http.get<Member[]>('http://localhost:9000/MEMBRESERVICE/membres')
   }
-  add( m: Member):Observable<void>{
-    return this.http.post<void>('http://localhost:3000/members',m);
+
+
+  getAllStudents():Observable<Etudiant[]>{
+    return this.http.get<Etudiant[]>('http://localhost:9000/MEMBRESERVICE/membres/etudiants')
   }
-  deleteMember( id: String):Observable<void>{
-    return this.http.delete<void>(`http://localhost:3000/members/${id}`); //pour ectraire l'id de l'url on doit utiliser `` et en cas de delete on doit envoyer l'id dand l'url
+
+  getAllTeachers():Observable<EnseignantChercheur[]>{
+    return this.http.get<EnseignantChercheur[]>('http://localhost:9000/MEMBRESERVICE/membres/teachers')
   }
-  getMemberById( id: String):Observable<Member>{
+
+
+  // type de retour dima observable khatr requette 
+  add(x:Member): Observable<void>
+  {
+  return this.http.post<void>('http://localhost:9000/MEMBRESERVICE/membres/enseignant',x)
+
+  }
+  delete(id:string): Observable<void>
+  {
+    return this.http.delete<void>(`http://localhost:3000/members/${id}`)
+  }
+  getMemberById(id:string):Observable<Member>
+  {
     return this.http.get<Member>(`http://localhost:3000/members/${id}`)
   }
-  updateMember(m: Member, id: string): Observable<void> {
-    return this.http.put<void>(`http://localhost:3000/members/${id}`, m);
+  updateMember(x:Member,id:string):Observable<void>{
+    return this.http.put<void>(`http://localhost:3000/members/${id}`,x)
   }
+ 
 }
